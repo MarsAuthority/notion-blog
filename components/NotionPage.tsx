91a6404846bcca457051a1691d47e3e8bea17010
MarsAuthority <chinamarsmail@gmail.com>
2023-02-09 23:33:04 +0800

    Update NotionPage.tsx
    update

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -21,7 +21,7 @@ import { useDarkMode } from '@/lib/use-dark-mode'
 
 import { Footer } from './Footer'
 import { ReactUtterances } from './ReactUtterances'
-// import { ReactCusdis } from 'react-cusdis'
+import { ReactCusdis } from 'react-cusdis'
 import { Loading } from './Loading'
 import { NotionPageHeader } from './NotionPageHeader'
 import { Page404 } from './Page404'
